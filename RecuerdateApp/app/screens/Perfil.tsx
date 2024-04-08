3b2e9f990828ccc97ebe045fcd1b3b9dc034c5f9
JuanR <112212338+JuanPython1@users.noreply.github.com>
2024-04-08 16:07:52 -0500

    Navegación Perfil

--- RecuerdateApp/app/screens/Perfil.tsx
+++ RecuerdateApp/app/screens/Perfil.tsx
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import { NavigationProp } from '@react-navigation/native';
 
-const Perfil = () => {
+interface RouterProps {
+  navigation: NavigationProp<any, any>;
+}
+
+
+const Perfil = ({navigation}: RouterProps ) => {
+
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Perfil</Text>
+      <Pressable
+              onPress={() => navigation.navigate('Mi tareas')}
+              style={({ pressed }) => {
+                return { opacity: pressed ? 0 : 1 };
+              }}>
+              <Image style={styles.icon} source={require('../../assets/icono.png')} />
+            </Pressable>
     </View>
   )
 }
 
 export default Perfil
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#acf9ff',
+    color: '#ffffff',
+  },  
+  icon: {
+    width: 45,
+    height: 45,
+    marginTop:20
+    // resizeMode: 'contain',
+  },
+})
