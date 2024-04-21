fc143f67962ecb143846d2c6a8f15e4bdab6281a
Daniela <112212338+JuanPython1@users.noreply.github.com>
2024-04-20 21:56:31 -0500

    Visualización Tareas
    Se llamo a las tareas creadas pero sin style.

--- RecuerdateApp/app/screens/Lista.tsx
+++ RecuerdateApp/app/screens/Lista.tsx
@@ -1,17 +1,40 @@
 import { NavigationProp } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { FIREBASE_AUTH } from '../../firebaseConfig';
+import { FIREBASE_AUTH, FIRESTORE_DB } from '../../firebaseConfig';
+import { collection, onSnapshot, deleteDoc, doc } from 'firebase/firestore';
+ // Importa la función de formato de fecha
 
 interface RouterProps {
   navigation: NavigationProp<any, any>;
 }
 
 const Lista = ({ navigation }: RouterProps) => {
+  const [tareas, setTareas] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(FIRESTORE_DB, 'Tareas'), (snapshot) => {
+      const updatedTareas = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setTareas(updatedTareas);
+      console.log(updatedTareas);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const goToTareas = () => {
-      navigation.navigate('CrearTarea')
+    navigation.navigate('CrearTarea')
   }
+
+  const eliminarTarea = async (id) => {
+    try {
+      await deleteDoc(doc(FIRESTORE_DB, 'Tareas', id));
+      const updatedTareas = tareas.filter((t) => t.id !== id);
+      setTareas(updatedTareas);
+    } catch (error) {
+      console.error('Error al eliminar la tarea:', error);
+    }
+  };
 
 
   return (
@@ -19,7 +42,6 @@ const Lista = ({ navigation }: RouterProps) => {
       <View style={styles.backImage}>
         <View style={styles.whiteBox}>
           <View style={styles.titleContainer}>
-          
             <Pressable
               onPress={() => FIREBASE_AUTH.signOut()}
               style={({ pressed }) => {
@@ -37,14 +59,39 @@ const Lista = ({ navigation }: RouterProps) => {
             </Pressable>
           </View>
           <Text style={styles.h2}>Mis Tareas</Text>
-
         </View>
         <Text style={styles.h3}> ^ Filtrar por Fecha</Text>
       </View>
-      
-      <TouchableOpacity style={styles.button} onPress={goToTareas}>
-                <Text style={styles.buttonText}>Agregar una nueva tarea</Text>
-      </TouchableOpacity>
+
+      <View>
+        <Text style={styles.h2}>Tareas</Text>
+        <View style={styles.tareasContainer}>
+          {tareas.map((tarea) => (
+            <View key={tarea.id} style={styles.tareaItem}>
+              <Text>Titulo: {tarea.Nombre}</Text>
+              <Text>{tarea.Descripcion}</Text>
+              <Text>{tarea.Prioridad}</Text>
+              <Text>{tarea.TipoTarea}</Text>
+              
+              <Pressable
+                onLongPress={() => eliminarTarea(tarea.id)} 
+                style={({ pressed }) => ({
+                  backgroundColor: pressed ? '#f00' : '#ccc', 
+                  padding: 10,
+                  borderRadius: 5,
+                  marginTop: 5,
+                })}>
+                <Text style={{ color: '#fff' }}>Eliminar</Text>
+              </Pressable>
+            </View>
+          ))}
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={goToTareas}>
+          <Text style={styles.buttonText}>Agregar una nueva tarea</Text>
+        </TouchableOpacity>
+
+      </View>
     </View>
   );
 };
@@ -61,7 +108,6 @@ const styles = StyleSheet.create({
   h1: {
     fontFamily:'Roboto',
     fontWeight:'bold',
-    // paddingTop: 2,
     fontSize: 20,
     textAlign:"center",
     alignItems: 'center',
@@ -70,7 +116,6 @@ const styles = StyleSheet.create({
   h2: {
     color: 'black',
     fontSize: 14,
-    // paddingTop: 2,
     textAlign:"center",
   },
   h3: {
@@ -107,8 +152,6 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     marginHorizontal: 65,
     alignItems: 'center',
-    // alignSelf: 'center',
-    // marginTop: 10
   },
   buttonText: {
     fontWeight:'bold',
@@ -121,6 +164,20 @@ const styles = StyleSheet.create({
     width: 45,
     height: 45,
     marginTop:20
-    // resizeMode: 'contain',
+  },
+  tareasContainer: {
+    backgroundColor: '#ffffff',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+  },
+  tareaItem: {
+    backgroundColor: '#fff',
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
